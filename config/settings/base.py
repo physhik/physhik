efdd7bf8ef5f050bcad3a452f4d5ad9ddf5598b6
physhik <physhik@gmail.com>
2025-01-31 13:55:39 -0800

    remove blog directory to start from scratch

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,6 +88,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "physhik.users",
     # Your stuff: custom apps go here
+    "physhik.blog",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
