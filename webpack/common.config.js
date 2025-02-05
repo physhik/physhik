4f40a93268057f20472467413b4cc1f7664229fa
physhik <physhik@gmail.com>
2025-02-05 10:44:47 -0800

    linting

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -10,10 +10,7 @@ module.exports = {
     vendors: path.resolve(__dirname, '../physhik/static/js/vendors'),
   },
   output: {
-    path: path.resolve(
-      __dirname,
-      '../physhik/static/webpack_bundles/',
-    ),
+    path: path.resolve(__dirname, '../physhik/static/webpack_bundles/'),
     publicPath: '/static/webpack_bundles/',
     filename: 'js/[name]-[fullhash].js',
     chunkFilename: 'js/[name]-[hash].js',
