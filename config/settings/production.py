d58604de5ad3bd27f89955e2d6f9a89e1609f291
physhik <physhik@gmail.com>
2025-02-11 09:57:57 -0800

    change the domain name, physhik.com to blog.physhik.com

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["blog.physhik.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["physhik.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -109,7 +109,7 @@ MEDIA_URL = f"https://{aws_s3_domain}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="physhik <noreply@physhik.com>",
+    default="physhik <noreply@mail.physhik.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
